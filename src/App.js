a4dd24d8b37d3483e7685ee1f89589a9ea9bc517
MEGEZR-R <hajwbehdudjend@gmail.com>
2024-10-31 16:35:29 +0300

    x2
    update  some assets and  pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-// src/App.js
-import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import SignInPage from './pages/signin';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<SignInPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+// pexels.com for free stock videos
+
+
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements'
+
+import { Button } from '../ButtonElement'
+
+const HeroSection = () => {
+
+    const [ hover, setHover ] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer id="home">
+            <HeroBg>
+                {/* for the header video or model */}
+            </HeroBg>
+            <HeroContent>
+                <HeroH1> Experience the guga </HeroH1>
+                <HeroP> 
+                    Sign up for a new account today and receive 
+                    free access to the world's first interactive guga.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button 
+                        to="signup" 
+                        onMouseEnter={ onHover } 
+                        onMouseLeave={ onHover }
+                        primary="true"
+                        dark="true"
+                        smooth={true}
+                        duration={500}
+                        spy={true}
+                        activeClass='active'
+                        exact='true'
+                        > 
+                        Get started 
+                        {hover ? <ArrowForward/> : <ArrowRight/>} 
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
